d9583aefb077ab9b0943125b361eecf6eab6313c
AY <alex.yicheng.yang@gmail.com>
2024-10-17 18:45:28 +0200

    request the version of python

---
+++ Object_2.py
@@ -0,0 +1,65 @@
+# 15.10.2024
+from tkinter.font import names
+
+
+class Person(object):
+    name = 'Sophia'
+    def __init__(self):
+        self.age = 18
+    def play(self):
+        # print(f'{self.name} plays game') # Option 1
+        print(f'{Person.name} plays game') # Option 2
+        print(self.age)
+    @staticmethod
+    def introduce():
+        print(f'I am {Person.name}')
+    @classmethod
+    def vorstellen(cls):
+        print(cls.name)
+
+# pe = Person()
+# pe.play()
+# pe.introduce()
+# pe.vorstellen()
+
+class Test(object):
+    def __init__(self):
+        print('It is __init__()')
+    def __new__(cls, *args, **kwargs):
+        print('__new__()')
+        # expand father class methode
+        res = super().__new__(cls)  #rewrite methode
+        return res
+        # import : rewrite __new__() have to use return super().__new__(cls) otherwise python doesnot have memory
+# te = Test()
+
+class Person(object):
+    def __new__(cls, *args, **kwargs):
+        print('it is __new__()')
+        return super().__new__(cls)
+    def __init__(self,name):
+        self.name = name
+        print('Name is',name)
+# pe = Person('Alex')
+
+# Summary
+# __new__() create object
+# __init__() init object
+
+class Singleton(object):
+    obj = None
+    def __new__(cls, *args, **kwargs):
+        print('That is __new__() function')
+        #if cls.obj is empty
+        if cls.obj == None:
+            cls.obj = super().__new__(cls)
+        return cls.obj
+    def __init__(self):
+        print('This is __init__()')
+
+# s = Singleton()
+# print('s:',s)
+# s2 = Singleton()
+# print('s2:',s2)
+
+
--- inherit.py
+++ inherit.py
@@ -33,9 +33,9 @@ class Son(Father):
 class Grandson(Son):
     pass
 
-grandson = Grandson()
-grandson.buy()
-grandson.eat()
+# grandson = Grandson()
+# grandson.buy()
+# grandson.eat()
 
 class Person:
     def money(self):
@@ -51,6 +51,80 @@ class Man(Person):
         #option 3
         # super(Man,self).money()
 
-m = Man()
-m.money()
+# m = Man()
+# m.money()
 
+#inherit multi father classes
+class Father(object):
+    def money(self):
+        print('100 Million')
+class Mother(object):
+    def money(self):
+        print('120 Million')
+    def appearance(self):
+        print('most beautiful')
+class Son(Mother,Father):
+    def money(self):
+        print('100 thousand')
+    pass
+
+
+# by multi father class has the same function -> use the closer father class
+# Son().money()
+# Son().appearance()
+# print(Son.__mro__)
+
+class Animal(object):
+    #father class : Animal
+    def shout(self):
+        print('Animal can shout')
+    def eat(self):
+        print('I can eat')
+class Cat(Animal):
+    #son class:cat
+    def shout(self):
+        print('Miao Miao Miao')
+class Dog(Animal):
+    #son class2:dog
+    def shout(self):
+        print('Wang Wang Wang')
+    def eat(self):
+        print('Dog eats dog food')
+class Pig(Animal):
+    def eat(self):
+        print('Pig eats pig food')
+
+# cat = Cat()
+# cat.shout()
+# dog = Dog()
+# dog.shout()
+
+
+def test(obj):
+    obj.eat()
+
+# animal = Animal()
+# test(animal)
+# pig = Pig()
+# test(pig)
+
+#static methode
+# @staticmethode without using self,cls variable
+
+class Person(object):
+    @staticmethod
+    def study(self):
+        print(f'{self} can learn')
+
+# Person.study('Alex')
+# pe = Person()
+# pe.study('Armin')
+
+class Person(object):
+    name = 'Thomas'
+    @classmethod
+    def sleep(cls):
+        print(cls)
+        print('Human sleeps')
+        print(cls.name)
+Person.sleep()
